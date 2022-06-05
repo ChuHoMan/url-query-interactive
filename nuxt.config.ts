17f9973fd94829bf74bc2d786f6a778552cf74ca
ChuHoMan <1075293216@qq.com>
2022-06-06 00:24:38 +0800

    chore: set baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/url-query-interactive/'
+    },
     typescript: {
         shim: false
     },
